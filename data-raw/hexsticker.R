3a2c2a13566175502da39b1d4d1076fdd4cc146a
comeetie <etienne.come@gmail.com>
2019-09-19 12:24:31 +0200

    optimal leaf ordering and cleaning

--- data-raw/hexsticker.R
+++
@@ -1,9 +0,0 @@
-library(hexSticker)
-imgurl <- "https://www.comeetie.fr/greed.jpg"
-library(showtext)
-## Loading Google fonts (http://www.google.com/fonts)
-font_add_google("Fredoka One", "fredoka")
-## Automatically use showtext to render text for future devices
-showtext_auto()
-sticker(imgurl, package="GREED", p_size=22, s_x=1, s_y=.95, s_width=.6,p_y=1.5,
-        filename="greed.png",h_color="#fe0096",h_fill="#ffffff",p_color="#000000",p_family = "fredoka")
